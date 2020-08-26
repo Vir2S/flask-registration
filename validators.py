de731521a74721482c3bbe05b392f668e91b9cc8
Vitaly Sem <virtualik@gmail.com>
2020-08-26 10:23:01 +0300

    validators fixed

--- validators.py
+++ validators.py
@@ -2,17 +2,12 @@ from validate_email import validate_email
 
 
 def check_username_validation(username):
-    if len(username) <= 4:
-        return False
-    return True
+    return False if len(username) <= 4 else True
 
 
 def check_email_validation(email):
-    valid = validate_email(email)
-    return valid
+    return validate_email(email)
 
 
 def check_password_validation(password):
-    if len(password) <= 6:
-        return False
-    return True
+    return False if len(password) <= 6 else True
